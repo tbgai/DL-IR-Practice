7e54bbc9c756f4e7ee11739c804ffabfeb74a32f
jingzl <jingzl@hotmail.com>
2019-12-09 22:50:33 +0800

    test MNIST dataset

--- KNN/MNIST.py
+++ KNN/MNIST.py
@@ -10,17 +10,20 @@ import torch
 from torch.utils.data import DataLoader
 import torchvision.datasets as dsets
 import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+
 batch_size = 100
 
-#MNIST dataset
-train_dataset = dsets.MNIST(root='C:\jingzl\workspace\GITHUB\KNN\ml\pymnist',
+# MNIST dataset
+# 下载一次即可，然后在本地直接使用
+train_dataset = dsets.MNIST(root='C:\jingzl\workspace\GITHUB\DL-IR-Practice\KNN\ml\pymnist',
                             train = True,
                             transform = None,
-                            download = True)
-test_dataset = dsets.MNIST( root='C:\jingzl\workspace\GITHUB\KNN\ml\pymnist',
+                            download = False) # download=True
+test_dataset = dsets.MNIST( root='C:\jingzl\workspace\GITHUB\DL-IR-Practice\KNN\ml\pymnist',
                            train = False,
                            transform = None,
-                           download = True)
+                           download = False) # download = True
 # 加载数据
 train_loader = torch.utils.data.DataLoader( dataset=train_dataset,
                                            batch_size = batch_size,
@@ -28,6 +31,15 @@ train_loader = torch.utils.data.DataLoader( dataset=train_dataset,
 test_loader = torch.utils.data.DataLoader( dataset=test_dataset, 
                                           batch_size=batch_size, 
                                           shuffle=True)
+print("train_data:", train_dataset.train_data.size())
+print("train_labels:", train_dataset.train_labels.size())
+print("test_data:", test_dataset.test_data.size())
+print("test_labels:",test_dataset.test_labels.size())
+
+digit = train_loader.dataset.train_data[0] # 取第一个图片的数据
+plt.imshow( digit, cmap=plt.cm.binary )
+plt.show()
+print( train_loader.dataset.train_labels[0]) # 输出对应的标签
 
 
 
